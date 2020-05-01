e55093227718bc6058025f75c4817bf847f902c2
Max <realmrmax@gmail.com>
2020-05-01 12:44:14 +0300

    'update'

--- plugin.rb
+++ plugin.rb
@@ -4,31 +4,31 @@
 # authors: Maxym Khaykin
 # url: https://github.com/realmrmax/discourse_topic_filter_by_user
 
-# after_initialize do
-  # require_dependency 'topic_query'
-
-  # if TopicQuery.respond_to?(:results_filter_callbacks)
-    # remove_muted_for_lists = [:latest, :new]
-    # remove_muted_tags = Proc.new do |list_type, result, user, options|
-
-        # muted_tags = DiscourseTagging.muted_tags(user)
-
-            # result.where("topics.user_id <> 1")
-
-    # end
-
-    # TopicQuery.results_filter_callbacks << remove_muted_tags
-  # end
-  
-# end
-
 after_initialize do
   require_dependency 'topic_query'
 
-		TopicQuery.add_custom_filter(:kb) do |results, topic_query|
+  if TopicQuery.respond_to?(:results_filter_callbacks)
+    remove_muted_for_lists = [:latest, :new]
+    remove_muted_tags = Proc.new do |list_type, result, user, options|
 
-			results = results.where("topics.user_id <> 2")
-		
-		end
- 
+        muted_tags = DiscourseTagging.muted_tags(user)
+
+            result.where("topics.user_id <> 1")
+
+    end
+
+    TopicQuery.results_filter_callbacks << remove_muted_tags
+  end
+  
 end
+
+# after_initialize do
+  # require_dependency 'topic_query'
+
+		# TopicQuery.add_custom_filter(:kb) do |results, topic_query|
+
+			# results = results.where("topics.user_id <> 2")
+		
+		# end
+ 
+# end
