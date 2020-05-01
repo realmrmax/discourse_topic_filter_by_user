7f00f29a85c24f79b9f6bd6d6c93113df65ca740
Max <realmrmax@gmail.com>
2020-05-01 18:32:37 +0300

    'update'

--- plugin.rb
+++ plugin.rb
@@ -6,31 +6,31 @@
 
 after_initialize do
   require_dependency 'topic_query'
-  require_dependency 'application_controller'
+  # require_dependency 'application_controller'
 
-  if TopicQuery.respond_to?(:results_filter_callbacks)
-    remove_muted_for_lists = [:latest, :new]
-    remove_muted_tags = Proc.new do |list_type, result, user, options|
+  # if TopicQuery.respond_to?(:results_filter_callbacks)
+    # remove_muted_for_lists = [:latest, :new]
+    # remove_muted_tags = Proc.new do |list_type, result, user, options|
 
-        muted_tags = DiscourseTagging.muted_tags(user)
-		#if  request.referer =~ /\// || request.referer =~ /\/categories\//
-            result.where("topics.user_id NOT IN (1,-1)")
-		#end
-		#Rails.logger.warn("#{Discourse.base_uri}")
-    end
+        # muted_tags = DiscourseTagging.muted_tags(user)
+		# #if  request.referer =~ /\// || request.referer =~ /\/categories\//
+            # result.where("topics.user_id NOT IN (1,-1)")
+		# #end
+		# #Rails.logger.warn("#{Discourse.base_uri}")
+    # end
 
-    TopicQuery.results_filter_callbacks << remove_muted_tags
-  end
+    # TopicQuery.results_filter_callbacks << remove_muted_tags
+  # end
   
-end
-
-# after_initialize do
-  # require_dependency 'topic_query'
-
-		# TopicQuery.add_custom_filter(:kb) do |results, topic_query|
-
-			# results = results.where("topics.user_id <> 2")
-		
-		# end
- 
 # end
+
+after_initialize do
+  require_dependency 'topic_query'
+
+		TopicQuery.add_custom_filter(:kb) do |results, latest|
+
+			results = results.where("topics.user_id NOT IN (1,-1)")
+		
+		end
+ 
+end
