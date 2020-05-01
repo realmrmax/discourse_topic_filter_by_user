9f684502171940c764ebc22aacc9f5f432f2298a
Max <realmrmax@gmail.com>
2020-05-01 10:47:06 +0300

    'initial'

---
+++ plugin.rb
@@ -0,0 +1,16 @@
+# name: discourse_topic_filter_by_user
+# about: Show latest filtered by user
+# version: 0.1
+# authors: Maxym Khaykin
+# url: https://github.com/realmrmax/discourse_topic_filter_by_user
+
+after_initialize do
+  require_dependency 'topic_query'
+
+		TopicQuery.add_custom_filter(:kb) do |results, topic_query|
+
+		 results = results.where("topics.used_id <> 1")
+		 results
+		end
+ 
+end
