cc51c55caea21c5d79e3b985eb13354a3c438723
Max <realmrmax@gmail.com>
2020-05-04 10:28:27 +0300

    'update'

--- plugin.rb
+++ plugin.rb
@@ -35,7 +35,7 @@ after_initialize do
 		# private
 		# def determine_website
 			# sitename = request.fullpath
-			Rails.logger.warn("#{params[:controller]}")
+			Rails.logger.warn("#{controller_name}")
 
 		# end
 
